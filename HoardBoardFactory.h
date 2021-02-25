c42750c1ad2a399d6d90125468e178443c8d4aa2
glee139 <glee00328@gmail.com>
2021-02-25 04:58:24 -0800

    added header guards to class

--- HoardBoardFactory.h
+++ HoardBoardFactory.h
@@ -1,7 +1,12 @@
+#ifndef HOARDBOARDFACTORY_HPP
+#define HOARDBOARDFACTORY_HPP
+
 class HoardBoardFactory : BoardFactory{
 	private:
 		pieces[][];
 	public:
 		HoardBoardFactory();
 		virtual Board createBoard();
-}
+};
+
+#endif
