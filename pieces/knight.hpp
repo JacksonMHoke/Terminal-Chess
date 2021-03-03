11dfe6265b4f828478850dae7c981734b4a6b000
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-02 17:15:08 -0800

    Implemented move function in all derived pieces
    * Added move function in these classes:
    	*pawn.hpp
    	*rook.hpp
    	*king.hpp
    	*queen.hpp
    	*knight.hpp
    	*bishop.hpp
    * Included stdexcept in piece.hpp
    * Added comments in derived piece classes for move function and
      set_behavior function

--- pieces/knight.hpp
+++ pieces/knight.hpp
@@ -6,7 +6,20 @@
 class Knight : public Piece {
     public:
         Knight(char l, char n, char c, char p, MovementBehavior* move=nullptr, Board* b=nullptr) : Piece(l, n, c, p, move, b) {}
-        //TODO move function
+        
+        //tries to move to (l,n) for all movement behaviors
+        //returns true if the move is valid, false if invalid
+        virtual bool move(char l, char n) {
+            if (moveB.size()==0) throw logic_error("Tried to move without movement behavior");
+            //tries all movement behavior to see if one works
+            //returns true if one works, false if all fail
+            for (int i=0; i<moveB.size(); ++i) {
+                if (moveB[i]->move(letter, number, l, n)) return true;
+            }
+            return false;
+        }
+
+        //sets movement behavior for pawn
         virtual bool move(const char l, const char n) { return true; }
         virtual void set_behavior(Board* b) {
             //moveB.push_back(new LMovement(b));
