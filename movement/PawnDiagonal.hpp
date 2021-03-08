ab77289b1365203590d87066a86bca0aca2a15ba
groovinwoovin <woovingroovin@yahoo.com>
2021-03-07 18:05:44 -0800

    Updated PawnDiag

--- movement/PawnDiagonal.hpp
+++ movement/PawnDiagonal.hpp
@@ -9,6 +9,7 @@ private:
 public:
 	virtual bool move(char x1, char y1, char x2, char y2) {
 		if(Piece::getColor(x1, y1) == 'b') {
+			if(board->getCell(x2, y2) == nullptr) { return false; }
 			if((abs(x1 - x2) == 1) && ((y1 - y2) == 1)) {
 				Piece::setCol(x2);
 				Piece::setRow(y2);
@@ -17,6 +18,7 @@ public:
 			else { return false; }
 		}
 		else {
+			if(board->getCell(x2, y2) == nullptr) { return false; }
                         if((abs(x1 - x2) == 1) && ((y1 - y2) == -1)) {
                                 Piece::setCol(x2);
                                 Piece::setRow(y2);
