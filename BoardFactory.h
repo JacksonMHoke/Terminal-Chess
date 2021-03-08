e9844333920588433320586eaae1d3d8d4baadb3
glee139 <glee00328@gmail.com>
2021-03-07 18:21:23 -0800

    updated the class methods

--- BoardFactory.h
+++ BoardFactory.h
@@ -3,27 +3,9 @@
 #include "Board.h"
 
 class BoardFactory : Game{
-	private:
-		pieces[][];
 	public:
 		BoardFactory();
 		virtual Board createBoard()=0;
-		void drawBoard(){
-			for (int i=0;i<pieces.length();i++)
-				for (int k=0;k<pieces.at(i).length(0);k++){
-					if (pieces.at(i).at(k)==nullptr)
-						cout << "* ";
-					cout << pieces.at(i).at(k).getPiece() << " ";
-				}
-				cout << endl;
-		}
-		//TODO
-		char getYLocation();
-		void setYlocation();
-		char getXLocation();
-		void setYlocation();
-		char getPiece();
-		bool isOccupied();
 };
 
 #endif
