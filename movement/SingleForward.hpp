951d2cd88350b62128ac6496256a09e544b5dcea
groovinwoovin <woovingroovin@yahoo.com>
2021-03-07 18:19:08 -0800

    Added SingleForward

--- movement/SingleForward.hpp
+++ movement/SingleForward.hpp
@@ -7,8 +7,27 @@ class SingleForward : public MovementBehavior {
 private:
 
 public:
-	//TODO - Implement requirements and actions
-	virtual bool move(char x1, char y1, char x2, char y2);
+	virtual bool move(char x1, char y1, char x2, char y2) {
+                if(((x1 - x2) == (y1 - y2)) && (x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
+			if(Piece::getColor() == 'b') {
+				if(((x1 - x2) == 0) && ((y1 - y2) == 1)) {
+					Piece::setCol(x2)
+					Piece::setRow(y2);
+					return true;
+				}
+				else { return false; }
+			}
+			else {
+                                if(((x1 - x2) == 0) && ((y1 - y2) == -1)) {
+					Piece::setCol(x2);
+					Piece::setRow(y2);
+					return true;			
+                                }
+				else { return false; }
+			}
+		}
+		else { return false; }
+        }
 }
 
 
