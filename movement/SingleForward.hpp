ca204b818c99d38c2c694bb1eb74fa56fb13ce43
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-08 18:22:36 -0800

    Fixed movement/SingleForward.hpp
    * Added constructor
    * move function accounts for check after movement now
    * added checks for if piece is in front of it

--- movement/SingleForward.hpp
+++ movement/SingleForward.hpp
@@ -7,30 +7,27 @@ class SingleForward : public MovementBehavior {
 private:
 
 public:
+	SingleForward(Board* b) : MovementBehavior(b) {}
 	virtual bool move(char x1, char y1, char x2, char y2) {
-                if(((x1 - x2) == (y1 - y2)) && (x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
-			if(Piece::getColor() == 'b') {
-				if(((x1 - x2) == 0) && ((y1 - y2) == 1)) {
-					Piece::setCol(x2)
-					Piece::setRow(y2);
+        if(x2 <= 'h' && x2 >= 'a' && y2 <= '8' && y2 >= '1') {
+			if(board->getCell(x1,y1)->getColor() == 'b') {
+				if(((x1 - x2) == 0) && ((y1 - y2) == 1) && board->getCell(x2,y2)==nullptr) {
+					if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+					board->move(x1, y1, x2, y2);
 					return true;
 				}
 				else { return false; }
 			}
 			else {
-                                if(((x1 - x2) == 0) && ((y1 - y2) == -1)) {
-					Piece::setCol(x2);
-					Piece::setRow(y2);
-					return true;			
-                                }
-				else { return false; }
+                if(((x1 - x2) == 0) && ((y1 - y2) == -1) && board->getCell(x2,y2)==nullptr) {
+					if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+					board->move(x1, y1, x2, y2);
+					return true;
+                } else { return false; }
 			}
 		}
 		else { return false; }
-        }
+    }
 }
-
-
-
 
 #endif
