cb476a9562342c7997c5966fa364795dbb4690be
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-08 18:37:59 -0800

    Fixed DiagonalMovement.hpp
    * Added constructor
    * fixed move function by accounting for all 4 diagonals instead of only
      up right
    * account for if same color piece is at destination
    * accounts for if king is in check after a move

--- movement/DiagonalMovement.hpp
+++ movement/DiagonalMovement.hpp
@@ -7,16 +7,34 @@ class DiagonalMovement : public MovementBehavior {
 private:
 
 public:
+	DiagonalMovement(Board* b) : MovementBehavior(b) {}
 	virtual bool move(char x1, char y1, char x2, char y2) {
+		//if there is same color piece at end coords
+		if (board->getCell(x2,y2)!=nullptr && board->getCell(x1, y1)->getColor()==board->getCell(x2, y2)->getColor()) return false;
+
+		//accounts for direction piece is moving in
+		bool l=false;
+		bool r=false;
+		bool u=false;
+		bool d=false;
+		if (x1>x2) l=true;
+		if (x1<x2) r=true;
+		if (y1<y2) u=true;
+		if (y1>y2) d=true;
+
 		// if the coordinates given are a diagonal and are within the bounds
-		if(((x1 - x2) == (y1 - y2)) && (x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
+		if(((x1 - x2) == (y1 - y2)) && (x2 <= 'h' && x2 >= 'a') && (y2 <= '8' && y2 >= '1')) {
 			// check if there is anything in the way of the diagonal move
-			for(int i = 1; i <= (x1 - x2); ++i) {
-				if(board->getCell(x1 + i, y1 + i) != nullptr) { return false; }
+			for(int i = 1; i <= abs(x1 - x2); ++i) {
+				if(u && r && board->getCell(x1 + i, y1 + i) != nullptr) { return false; }
+				if(u && l && board->getCell(x1 - i, y1 + i) != nullptr) { return false; }
+				if(d && l && board->getCell(x1 - i, y1 - i) != nullptr) { return false; }
+				if(d && r && board->getCell(x1 + i, y1 - i) != nullptr) { return false; }
 			}
+
 			// checked if anything is in the way, now move
-			Piece::setCol(x2);
-			Piece::setRow(y2);
+			if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+			board->move(x1, y1, x2, y2);
 			return true;
 		}
 	}
