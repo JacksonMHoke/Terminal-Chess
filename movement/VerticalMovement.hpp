e5955e49bebff6787e593b11bb39d2aeeb4683ff
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-08 18:12:25 -0800

    Fixed VerticalMovement.hpp
    * Fixed broken syntax
    * accounted for checks after moving
    * accounted for if same color piece was at destination
    * fixed coords(x was number when x was meant to be l, vice versa)

--- movement/VerticalMovement.hpp
+++ movement/VerticalMovement.hpp
@@ -7,35 +7,35 @@ class VerticalMovement : public MovementBehavior {
 private:
 
 public:
+	VerticalMovement(Board* b) : MovementBehavior(b) {}
 	virtual bool move(char x1, char y1, char x2, char y2) {
+		//if there is same color piece at end coords
+		if (board->getCell(x2,y2)!=nullptr && board->getCell(x1, y1)->getColor()==board->getCell(x2, y2)->getColor()) return false;
 		// if in boundary
-		if((x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
+		if((x2<='h' && x2>='a') && (y2 <= '8' && y2 >= '1')) {
 			// downwards movement
 			if(((x1 - x2) == 0) && ((y1 - y2) > 0)){
 				// check if anything interferes
 				for(int i = 0; i < (y1 - y2); ++i) {
 					if(board->getCell(x1, y1 - i) != nullptr) { return false; }
 				}
-				Piece::setCol(x2);
-				Piece::setRow(y2);
+				if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+				board->move(x1, y1, x2, y2);
 				return true;
 			}
 			// upwards movement
 			else if (((x1 - x2) == 0) && ((y1 - y2) < 0)) {
-                                // check if anything interferes
-                                for(int i = 0; i < abs(y1 - y2); ++i) {
-                                        if(board->getCell(x1, y1 + i) != nullptr) { return false; }
-                                }
-                                Piece::setCol(x2);
-                                Piece::setRow(y2);
-                                return true;
+				// check if anything interferes
+				for(int i = 0; i < abs(y1 - y2); ++i) {
+					if(board->getCell(x1, y1 + i) != nullptr) { return false; }
+				}
+				if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+				board->move(x1, y1, x2, y2);
+				return true;
 			}
 		}
 		else { return false; }
 	}
-}
-
-
-
+};
 
 #endif
