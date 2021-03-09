b2981a275e5cf8c7739e812eef9f7651ef45fb7f
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-08 18:27:54 -0800

    Fixed HorizontalMovement
    * Added constructor
    * checks for same color piece at destination now
    * checks for check after moving
    * swapped coords for x and y to what they should be

--- movement/HorizontalMovement.hpp
+++ movement/HorizontalMovement.hpp
@@ -7,17 +7,20 @@ class HorizontalMovement : public MovementBehavior {
 private:
 
 public:
+        HorizontalMovement(Board* b) : MovementBehavior(b) {}
         virtual bool move(char x1, char y1, char x2, char y2) {
+                //if there is same color piece at end coords
+		if (board->getCell(x2,y2)!=nullptr && board->getCell(x1, y1)->getColor()==board->getCell(x2, y2)->getColor()) return false;
                 // if in boundary
-		if((x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
+		if((x2 <= 'h' && x2 >= 'a') && (y2 <= '8' && y2 >= '1')) {
                         // leftwards movement
 			if(((x1 - x2) > 0) && ((y1 - y2) == 0)){
                                 // check if anything interferes
 				for(int i = 0; i < (x1 - x2); ++i) {
                                         if(board->getCell(x1 - i, y1) != nullptr) { return false; }
                                 }
-                                Piece::setCol(x2);
-                                Piece::setRow(y2);
+                                if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+				board->move(x1, y1, x2, y2);
                                 return true;
                         }
                         // rightwards movement
@@ -26,16 +29,13 @@ public:
                                 for(int i = 0; i < abs(x1 - x2); ++i) {
                                         if(board->getCell(x1 + i, y1) != nullptr) { return false; }
                                 }
-                                Piece::setCol(x2);
-                                Piece::setRow(y2);
+                                if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+				board->move(x1, y1, x2, y2);
                                 return true;
                         }
 		}
 		else { return false; }
 	}
-}
-
-
-
+};
 
 #endif
