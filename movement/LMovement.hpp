8cd11b1e754762d374201ab8e4a3cc0e4a995e90
JacksonMHoke <jacksonmhoke@gmail.com>
2021-03-08 18:43:17 -0800

    Fixed LMovement.hpp
    * Added constructor
    * now checks for if king is in check after a move
    * now checks if destination is occupied by same color piece

--- movement/LMovement.hpp
+++ movement/LMovement.hpp
@@ -7,21 +7,22 @@ class LMovement : public MovementBehavior {
 private:
 
 public:
-        virtual bool move(char x1, char y1, char x2, char y2) {
+	LMovement(Board* b) : MovementBehavior(b) {}
+    virtual bool move(char x1, char y1, char x2, char y2) {
+		//if there is same color piece at end coords
+		if (board->getCell(x2,y2)!=nullptr && board->getCell(x1, y1)->getColor()==board->getCell(x2, y2)->getColor()) return false;
+
 		// Check if the movement is an L
 		if(((abs(x1 - x2) == 2) && (abs(y1 - y2) == 1)) || (((abs(x1 - x2) == 1) && (abs(y1 - y2) == 2)))) {
 			if((x2 <= 8 && x2 >= 1) && (y2 <= 'h' && y2 >= 'a')) {
-				Piece::setCol(x2);
-				Piece::setRow(y2);
+				if (isCheck(board->getCell(x1, y1)->getColor())) return false;
+				board->move(x1, y1, x2, y2);
 				return true;
 			}
 			else { return false; }
 		}
 		else { return false; }
 	}
-}
-
-
-
+};
 
 #endif
