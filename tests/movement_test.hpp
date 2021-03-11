8bda99022f61c4702c7f634576e1a31e37805196
groovinwoovin <woovingroovin@yahoo.com>
2021-03-10 20:25:01 -0800

    Added movement functions

--- tests/movement_test.hpp
+++ tests/movement_test.hpp
@@ -12,52 +12,133 @@
 #include <string>
 #include <vector>
 
-TEST(MovementBehavior, PawnMove) {
-	EXPECT_EQ(false, true);	
+TEST(MovementBehavior, PawnsMove) {
+        Board* b = new Board();
+        b->addPiece(new Pawn('c', '2', 'w', 'P', b));
+        b->addPiece(new Pawn('c', '7', 'b', 'p', b));
+
+        ASSERT_EQ(true, b->getCell('c', '2')->move('c', '3'));
+        ASSERT_EQ(true, b->getCell('c', '7')->move('c', '5'));
+        delete b;
 }
 
 TEST(MovementBehavior, RookMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Rook('a', '1', 'w', 'R', b));
+        b->addPiece(new Rook('a', '8', 'b', 'r', b));
+
+        ASSERT_EQ(true, b->getCell('a', '1')->move('a', '7'));
+        ASSERT_EQ(false, b->getCell('a', '8')->move('a', '3'));
+        delete b;
 }
 
 TEST(MovementBehavior, BishopMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Bishop('c', '1', 'w', 'B', b));
+        b->addPiece(new Bishop('c', '8', 'b', 'b', b));
+
+        ASSERT_EQ(true, b->getCell('c', '1')->move('e', '3'));
+        ASSERT_EQ(true, b->getCell('c', '8')->move('g', '4'));
+        delete b;
 }
 
 TEST(MovementBehavior, KnightMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Knight('a', '5', 'w', 'N', b));
+
+        ASSERT_EQ(true, b->getCell('a', '5')->move('b', '7'));
+        ASSERT_EQ(true, b->getCell('b', '7')->move('d', '8'));
+        ASSERT_EQ(true, b->getCell('d', '8')->move('f', '7'));
+        ASSERT_EQ(true, b->getCell('f', '7')->move('g', '5'));
+        ASSERT_EQ(true, b->getCell('g', '5')->move('f', '3'));
+        ASSERT_EQ(true, b->getCell('f', '3')->move('d', '2'));
+        ASSERT_EQ(true, b->getCell('d', '2')->move('b', '3'));
+        ASSERT_EQ(true, b->getCell('b', '3')->move('a', '5'));
+        delete b;
 }
 
 TEST(MovementBehavior, KingMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new King('d', '4', 'w', 'K', b));
+
+        ASSERT_EQ(true, b->getCell('d', '4')->move('d', '5'));
+        ASSERT_EQ(true, b->getCell('d', '5')->move('d', '4'));
+        ASSERT_EQ(true, b->getCell('d', '4')->move('c', '4'));
+        ASSERT_EQ(true, b->getCell('c', '4')->move('d', '4'));
+        ASSERT_EQ(true, b->getCell('d', '4')->move('e', '5'));
+        ASSERT_EQ(true, b->getCell('e', '5')->move('f', '4'));
+        ASSERT_EQ(true, b->getCell('f', '4')->move('e', '3'));
+        ASSERT_EQ(true, b->getCell('e', '3')->move('d', '4'));
+        delete b;
 }
 
 TEST(MovementBehavior, QueenMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Queen('d', '4', 'w', 'Q', b));
+
+        ASSERT_EQ(true, b->getCell('d', '4')->move('d', '6'));
+        ASSERT_EQ(true, b->getCell('d', '6')->move('d', '4'));
+        ASSERT_EQ(true, b->getCell('d', '4')->move('b', '4'));
+        ASSERT_EQ(true, b->getCell('b', '4')->move('d', '4'));
+        ASSERT_EQ(true, b->getCell('d', '4')->move('f', '6'));
+        ASSERT_EQ(true, b->getCell('f', '6')->move('h', '4'));
+        ASSERT_EQ(true, b->getCell('h', '4')->move('f', '2'));
+        ASSERT_EQ(true, b->getCell('f', '2')->move('d', '4'));
+        delete b;
 }
 
 TEST(MovementBehavior, InvalidPawnMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Pawn('c', '2', 'w', 'P', b));
+        b->addPiece(new Pawn('c', '7', 'b', 'p', b));
+
+        ASSERT_EQ(false, b->getCell('c', '2')->move('c', '5'));
+        ASSERT_EQ(false, b->getCell('c', '7')->move('c', '8'));
+        delete b;
 }
 
 TEST(MovementBehavior, InvalidRookMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Rook('a', '1', 'w', 'R', b));
+        b->addPiece(new Rook('a', '8', 'b', 'r', b));
+
+        ASSERT_EQ(false, b->getCell('a', '1')->move('b', '7'));
+        ASSERT_EQ(false, b->getCell('a', '8')->move('c', '3'));
+        delete b;
 }
 
 TEST(MovementBehavior, InvalidBishopMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Bishop('c', '1', 'w', 'B', b));
+        b->addPiece(new Bishop('c', '8', 'b', 'b', b));
+
+        ASSERT_EQ(false, b->getCell('c', '1')->move('e', '0'));
+        ASSERT_EQ(false, b->getCell('c', '8')->move('c', '4'));
+        delete b;
 }
 
 TEST(MovementBehavior, InvalidKnightMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Knight('a', '5', 'w', 'N', b));
+
+        ASSERT_EQ(false, b->getCell('a', '5')->move('b', '8'));
+	delete b;
 }
 
 TEST(MovementBehavior, InvalidKingMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new King('d', '4', 'w', 'K', b));
+
+        ASSERT_EQ(false, b->getCell('d', '4')->move('d', '6'));
+	delete b;
 }
 
 TEST(MovementBehavior, InvalidQueenMove) {
-        EXPECT_EQ(false, true);
+        Board* b = new Board();
+        b->addPiece(new Queen('d', '4', 'w', 'Q', b));
+
+        ASSERT_EQ(false, b->getCell('d', '4')->move('e', '7'));
+	delete b;
 }
 
 #endif
